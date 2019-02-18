1fa9b3b11821932d68e436d35d2559148256dd6f
sailesh31 <32615753+sailesh31@users.noreply.github.com>
2019-02-18 18:53:29 +0530

    Add files via upload
    Connecting to Wifi

---
+++ Wifi_Mq2.ino
@@ -0,0 +1,41 @@
+#include<ESP8266WiFi.h>
+
+const char* ssid = "sailesh";
+const char* password = "12345678" ;
+
+int ledPin = 13;
+
+
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPin,LOW);
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Wifi connecting");
+
+  WiFi.begin(ssid,password);
+
+  Serial.println();
+
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    
+  }
+
+  digitalWrite(ledPin,HIGH);
+  Serial.println();
+
+  
+  }
+
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+
+}
